69eb03b9ae87d31ef0fbf83a322927587c184a78
katmoraesx <kathelyn_andrade23@icloud.com>
2025-08-08 14:14:44 -0300

    subindo

--- src/pages/Kelvin.jsx
+++ src/pages/Kelvin.jsx
@@ -8,13 +8,12 @@ export default function Colega() {
       <div className="colega-card">
         <div className="colega-info">
           <p className="intro-text">
-            "Olá! Meu nome é **Kelvin**. Fico feliz que você tenha gostado do livro do Homem-Aranha. Eu o escolhi porque a história de Peter Parker vai muito além dos superpoderes. É sobre responsabilidade, luta e a jornada de um jovem comum que se torna extraordinário. É uma leitura que nos inspira a encontrar o herói que existe em todos nós."
+            Obrigada pela indicação, Kelvin! Eu ja havia assistido aos filmes antes, mas ler o livro foi uma maneira incrível de imaginar a história com cenários criados pela minha cabeça. 
           </p>
         </div>
         
         <div className="spidey-icon-wrapper">
           <i className="fas fa-hand-spock"></i>
-          <p className="spidey-greetings">EXCELSIOR!</p>
         </div>
       </div>
     </div>
